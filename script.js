2f287f48aba6a2898917fbd1ab46e1edbff6f511
AnkurGarg07git config --global user.email Ankurgarg89p@gmail.comgit config --listgit config --global user.name AnkurGarg07 <Ankurgarg89p@gmail.com>
2023-12-19 15:27:57 +0530

    responsive features added

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function scrollToSection(sectionId) {
         section.scrollIntoView({ behavior: "smooth" });
     }
 }
+function SendEmail(){
+Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "username",
+    Password : "password",
+    To : 'them@website.com',
+    From : "you@isp.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+}).then(
+  message => alert(message)
+);
+}
